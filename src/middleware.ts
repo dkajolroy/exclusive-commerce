d75c0a437330f5253b937cc886f03d3ead842b5f
Kajol Roy <shrikajol@gmail.com>
2023-10-09 09:25:57 +0600

    proteced page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,24 @@
 import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req: NextRequestWithAuth) {
-    // Call
+    const { pathname, origin } = req.nextUrl;
+    const { token } = req.nextauth;
+    // already authenticated to redirect home from login page
+    if (pathname.startsWith("/auth") && token) {
+      NextResponse.redirect(new URL("/", origin));
+    }
   },
   {
     callbacks: {
       authorized: ({ token, req }) => {
-        // if (req.nextUrl.pathname.startsWith("/auth")) return true;
+        if (req.nextUrl.pathname.startsWith("/auth")) return true;
         return !!token;
       },
     },
   }
 );
 export const config = {
-  matcher: ["/cart", "/dashboard/:path*"],
+  matcher: ["/cart", "/auth/:path*", "/dashboard/:path*"],
 };
